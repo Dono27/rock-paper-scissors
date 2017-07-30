e2a13b438c5e88204ee004f5285b945c34d71d2e
Bence László <laszlo.bence.27@gmail.com>
2017-07-30 17:31:56 +0200

    Complete Refresh
    - much better and cleaner code
    - new, simplified icons
    - removed author's spam

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # rock-paper-scissors
-The popular rock-paper-scissors game in Python 3 with tkinter.
-Feel free to
-- contribute
-- fork
-- use
-- suggest changes
+Rock-paper-scissors game in Python 3 with tkinter and custom icons.
--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,87 +1,71 @@
-# rock-paper-scissors by Dono27
-# Bence László
-# www.bencelaszlo.cf
+#!/usr/bin/python3
 from random import *
 from tkinter import *
 
 #choice section
-def user_choice_rock():
-    user_choice = "rock"
-    turn(user_choice)
-    user_image.configure(image=rock_image)
-def user_choice_paper():
-    user_choice = "paper"
-    turn(user_choice)
-    user_image.configure(image=paper_image)
-def user_choice_scissors():
-    user_choice = "scissors"
-    turn(user_choice)
-    user_image.configure(image=scissors_image)
+def userChoiceRock():
+    userChoice = "rock"
+    turn(userChoice)
+    userImage.configure(image = rockImage)
+
+def userChoicePaper():
+    userChoice = "paper"
+    turn(userChoice)
+    userImage.configure(image = paperImage)
+
+def userChoiceScissors():
+    userChoice = "scissors"
+    turn(userChoice)
+    userImage.configure(image = scissorsImage)
 
 #gameplay setion
-def turn(user_choice):
-    oppo = ['rock', 'paper', 'scissors']
-    oppo_choice=oppo[randint(0,2)]
-    if(oppo_choice=='rock'):
-        oppo_image.configure(image=rock_image)
-        if(user_choice=='paper'):
-            turn_result.configure(text="You Win!", fg="green")
-            compare.configure(text=">")
-        elif(user_choice=='scissors'):
-            turn_result.configure(text="You are defeated!", fg="red")
-            compare.configure(text="<")
-        else:
-            turn_result.configure(text="It is a draw.", fg="gray")
-            compare.configure(text="=")
-            
-    elif(oppo_choice=='paper'):
-        oppo_image.configure(image=paper_image)
-        if(user_choice=='scissors'):
-            turn_result.configure(text="You Win!", fg="green")
-            compare.configure(text=">")
-        elif(user_choice=='rock'):
-            turn_result.configure(text="You are defeated!", fg="red")
-            compare.configure(text="<")
-        else:
-            turn_result.configure(text="It is a draw.", fg="gray")
-            compare.configure(text="=")
-            
-    elif(oppo_choice=='scissors'):
-        oppo_image.configure(image=scissors_image)
-        if(user_choice=='rock'):
-            turn_result.configure(text="You Win!", fg="green")
-            compare.configure(text=">")
-        elif(user_choice=='paper'):
-            turn_result.configure(text="You are defeated!", fg="red")
-            compare.configure(text="<")
-        else:
-            turn_result.configure(text="It is a draw.", fg="gray")
-            compare.configure(text="=")
-            
-#main program
-main_window = Tk()
-main_window.title("Rock-Paper-Scissors - Bence László - www.bencelaszlo.cf")
-rock_button = Button(main_window, width=20, text="ROCK!", justify=CENTER, command=user_choice_rock, activebackground='black', activeforeground='white')
-paper_button = Button(main_window, width=20, text="PAPER!", justify=CENTER, command=user_choice_paper, activebackground='black', activeforeground='white')
-scissors_button = Button(main_window, width=20, text="SCISSORS!", justify=CENTER, command=user_choice_scissors, activebackground='black', activeforeground='white')
-rock_image = PhotoImage(file="dono27_rps/rock.gif")
-paper_image = PhotoImage(file="dono27_rps/paper.gif")
-scissors_image = PhotoImage(file="dono27_rps/scissors.gif")
-user_image = Label(image=rock_image)
-user_image.image = rock_image
-compare = Label(main_window, justify=CENTER, font=("Helvetica", 30))
-oppo_image = Label(image=paper_image)
-oppo_image.image = paper_image
-turn_result = Label(main_window, width=20, justify=CENTER, font=("Helvetica", 20))
+def turn(userChoice):
+    opponent = ['rock', 'paper', 'scissors']
+    opponentChoice = opponent[randint(0,2)]
 
-#create grid
-rock_button.grid(row=2, column=1)
-paper_button.grid(row=2, column=2)
-scissors_button.grid(row=2, column=3)
-user_image.grid(row=3, column=1)
-compare.grid(row=3, column=2)
-oppo_image.grid(row=3, column=3)
-turn_result.grid(row=4, column=2)
+    if(opponentChoice == 'rock'):
+        opponentImage.configure(image = rockImage)
+    elif(opponentChoice == 'paper'):
+        opponentImage.configure(image = paperImage)
+    else:
+        opponentImage.configure(image = scissorsImage)
+
+    if(opponentChoice == userChoice):
+        turnResult.configure(text = "It's a draw.", fg = "gray")
+        compare.configure(text = "=")
+    elif((opponentChoice == 'rock' and userChoice == 'scissors') or (opponentChoice == 'paper' and userChoice == 'rock') or (opponentChoice == 'scissors' and userChoice == 'paper')):
+        turnResult.configure(text = "You are defeated!", fg = "red")
+        compare.configure(text="<")
+    else:
+        turnResult.configure(text = "You win!", fg = "green")
+        compare.configure(text = ">")
+
+#main program
+mainWindow = Tk()
+mainWindow.title("Rock-Paper-Scissors by Nrehtron")
+rockButton = Button(mainWindow, width = 20, text = "ROCK!", justify = CENTER, command = userChoiceRock, activebackground = 'black', activeforeground = 'white')
+paperButton = Button(mainWindow, width = 20, text = "PAPER!", justify = CENTER, command = userChoicePaper, activebackground = 'black', activeforeground = 'white')
+scissorsButton = Button(mainWindow, width = 20, text = "SCISSORS!", justify = CENTER, command = userChoiceScissors, activebackground = 'black', activeforeground = 'white')
+
+#images
+rockImage = PhotoImage(file = "nrehtron/rps/rock.gif")
+paperImage = PhotoImage(file = "nrehtron/rps/paper.gif")
+scissorsImage = PhotoImage(file = "nrehtron/rps/scissors.gif")
+userImage = Label(image = rockImage)
+userImage.image = rockImage
+compare = Label(mainWindow, justify = CENTER, font = ("Helvetica", 30) )
+opponentImage = Label(image = paperImage)
+opponentImage.image = paperImage
+turnResult = Label(mainWindow, width = 20, justify = CENTER, font = ("Helvetica", 20) )
+
+#Tk GUI grid
+rockButton.grid(row = 2, column = 1)
+paperButton.grid(row = 2, column = 2)
+scissorsButton.grid(row = 2, column = 3)
+userImage.grid(row = 3, column = 1)
+compare.grid(row = 3, column = 2)
+opponentImage.grid(row = 3, column = 3)
+turnResult.grid(row = 4, column = 2)
 
 #mainloop
-main_window.mainloop()
+mainWindow.mainloop()
